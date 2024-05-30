f605979f42030d457aa61f3cb56b7ea796a59d64
rohullah <rohullah.khorami98@gmail.com>
2024-05-30 11:47:05 +0200

    more math added

--- arthematicExpression.sh
+++ arthematicExpression.sh
@@ -7,8 +7,31 @@
 # solving Math expression 
 #
 #################################################################
-read math
-output=$(echo "$math" | bc -l)
-rounded_output=$(printf "%.3f" "$output")
 
-echo "$rounded_output"
+getNinputs(){
+    read N
+
+    nn=$(echo "$N" | awk '{print int($1)}')
+
+    result=0
+
+    for ((i = 0; i <nn; i++)); do
+        read temp
+        temp1=$(echo "$temp" | awk '{print int($1)}')
+        result=$((result + temp1))
+    done
+
+
+    final=$(echo "scale=3; $result / $nn" | bc -l)
+    formatedOutput=$(printf "%.3f" "$final")
+    echo "$formatedOutput"
+}
+expression(){
+    read math
+    output=$(echo "$math" | bc -l)
+    rounded_output=$(printf "%.3f" "$output")
+
+    echo "$rounded_output"
+}
+
+
